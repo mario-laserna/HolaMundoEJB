14de238bcb9cb058a46e7e3fb8701e45b44ac65c
Mario Laserna <laserna.mario@gmail.com>
2015-03-25 14:30:54 -0500

    Se agrega ejb stateless y prueba unitaria con junit

---
+++ src/main/java/beans/HolaMundoEJB.java
@@ -0,0 +1,12 @@
+package beans;
+
+import javax.ejb.Stateless;
+
+@Stateless
+public class HolaMundoEJB {
+	
+	public int sumar(int a, int b){
+		return a + b;
+	}
+
+}
---
+++ src/test/java/test/HolaMundoEJBTest.java
@@ -0,0 +1,39 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+
+import javax.ejb.embeddable.EJBContainer;
+import javax.naming.Context;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import beans.HolaMundoEJB;
+
+public class HolaMundoEJBTest {
+	private static EJBContainer contenedor;
+	private static Context contexto;
+	private static HolaMundoEJB ejb;
+	
+	@Before
+	public void iniciarContendor() throws Exception{
+		System.out.println("Iniciando EJB container");
+		contenedor = EJBContainer.createEJBContainer();
+		contexto = contenedor.getContext();
+		ejb = (HolaMundoEJB)contexto.lookup("java:global/classes/HolaMundoEJB!beans.HolaMundoEJB");
+	}
+	
+	@Test
+	public void testAddNumbers() throws Exception{
+		int dato1 = 3;
+		int dato2 = 6;
+		
+		int resultado = ejb.sumar(dato1, dato2);
+		assertEquals((dato1+dato2),resultado);
+		System.out.println("Operación terminada, resultado: " + resultado);
+		
+	}
+	
+	
+
+}
